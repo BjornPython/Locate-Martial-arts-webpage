ce3640a3c5bd3d69e22037f3702d4643527a9363
Nathan <nathanflores887@gmail.com>
2023-01-19 10:38:29 +0800

    Added dropdowns for what art is the user searching for.

--- frontend/src/public/comps/loggedin/UprofileFinding.jsx
+++ frontend/src/public/comps/loggedin/UprofileFinding.jsx
@@ -1,22 +1,51 @@
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import uuid from 'react-uuid'
+const displayMart = (mart) => {
+    return (
+        <div className='u-display-mart' key={uuid()} onClick={() => { console.log(`${mart} clicked`) }}>
+            <span className='u-mart-span'></span>
+            <h4>{mart}</h4>
+        </div>
+    )
+}
+
 
 function UprofileFinding() {
+    const [spartnerIn, setSpartnerIn] = useState(["Kickboxing", "Muay thai", "BJJ"])
     return (
         <div className="looking-for">
             <div className="u-for">
                 <h4>Looking for a Sparring Partner:</h4>
-                <h4 className='looking-for-dropdown'>Sparring partner in...  <FontAwesomeIcon icon={faCaretDown} /></h4>
-                <div className="spar-dropdown">
+                <div className='dropdowns-div'>
+                    <div className='looking-for-dropdown'>
+                        <h4>Sparring partner in...</h4>
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </div>
+
+                    <div className="spar-dropdown">
+                        {spartnerIn.map(mart => { return displayMart(mart) })}
+                    </div>
                 </div>
+
             </div>
 
             <div className="u-for">
                 <h4>Looking for a Coach:</h4>
-                <h4 className='looking-for-dropdown'>Coach in...  <FontAwesomeIcon icon={faCaretDown} /></h4>
-                <div className="spar-dropdown">
+                <div className='dropdowns-div'>
+                    <div className='looking-for-dropdown'>
+                        <h4>Sparring partner in...</h4>
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </div>
+
+                    <div className="spar-dropdown">
+                        {spartnerIn.map(mart => { return displayMart(mart) })}
+                    </div>
                 </div>
+
             </div>
+
         </div>
     )
 }
