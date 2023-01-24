3de0a617fb405feb3ac114d0fe48a9fd1caac86d
Nathan <nathanflores887@gmail.com>
2023-01-24 17:24:38 +0800

    Added some new designs to UprofileBox.jsx

--- frontend/src/public/comps/loggedin/UprofileBox.jsx
+++ frontend/src/public/comps/loggedin/UprofileBox.jsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faGear } from '@fortawesome/free-solid-svg-icons'
-
-function UprofileBox({ name, bio }) {
-    return (<div className='profile-box'>
+import { useState } from 'react'
+const showNameBio = (name, bio) => {
+    return (
         <div className='profile'>
             <FontAwesomeIcon icon={faUser} className="profile-avatar" />
             <div className='profile-info'>
@@ -10,9 +10,33 @@ function UprofileBox({ name, bio }) {
                 <p>{bio === "" || bio === undefined ? "Edit your bio" : bio}</p>
             </div>
         </div>
-        <FontAwesomeIcon icon={faGear} className="u-setting-icon" />
+    )
+}
 
-    </div>);
+const showEditableNameBio = (name, bio) => {
+    return (
+        <div className='profile'>
+            <FontAwesomeIcon icon={faUser} className="profile-avatar" />
+            <div className='profile-info'>
+                <input type="text" className='i-name' name="i-name" id="" placeholder={name} />
+                <input type="text" className='i-bio' name="i-bio" id="" placeholder={bio !== "" ? bio : "Edit your bio"} />
+
+            </div>
+        </div>
+    )
+}
+
+function UprofileBox({ name, bio }) {
+
+    const [showEditable, setShowEditable] = useState(false)
+
+    return (
+        <div className='profile-box'>
+            {showEditableNameBio(name, bio, showEditable)}
+            <FontAwesomeIcon icon={faGear} className="u-setting-icon" onClick={() => { setShowEditable(!showEditable) }} />
+
+        </div>
+    );
 }
 
 export default UprofileBox
