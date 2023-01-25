fa25f371b6b7d8ddb38298aee9abb5b76b56112c
Nathan <nathanflores887@gmail.com>
2023-01-25 14:06:50 +0800

    Removed console logs in Uprofile.jsx

--- frontend/src/public/comps/loggedin/Uprofile.jsx
+++ frontend/src/public/comps/loggedin/Uprofile.jsx
@@ -11,7 +11,6 @@ import UprofileBox from "./UprofileBox";
 import UprofileStatus from './UprofileStatus';
 import UprofileFinding from './UprofileFinding'
 import UprofileContents from './UprofileContents'
-import { faAppStoreIos } from '@fortawesome/free-brands-svg-icons'
 
 
 function Uprofile({ user, info }) {
@@ -27,19 +26,6 @@ function Uprofile({ user, info }) {
     const { addMart, addAward } = newInfo
     // Used when the dom is first loaded. will only display userInfo once the
     // user's information from the backend is received and set.
-    const [displayInfo, setDisplayInfo] = useState(false)
-    // gets the user's information by requesting a GET request to the backend.
-
-
-
-    const getUserInfo = async () => {
-        console.log("USER TOKEN: ", user);
-        console.log("GETTING USER INFO");
-        const response = await apiService.getUserInfo(user);
-        setNewUserInfo({ ...response.data, lfDataChanged: 0 });
-        // had to stringiny then parse so the two states will not have the same reference.
-        setDbUserInfo(JSON.parse(JSON.stringify(response.data)));
-    }
 
 
     // has the initial value of userInfo. information here will be displayed in the
@@ -72,19 +58,10 @@ function Uprofile({ user, info }) {
     // Calls the getUserInfo function to get and set the user's information. also sets the 
     // setDisplayInfo to true so the profile will display the information.
     useEffect(() => {
-        console.log("INFO CHANGED IN UPROFILE: ", info);
         if (info === null) {
-            console.log("INFO IN UPROFILE IS NULL");
             return
-            // console.log("SETTING PROFULE DATA");
-            // const setProfileData = async () => {
-            //     await getUserInfo();
-            //     setDisplayInfo(true)
-            // }
-            // setProfileData()
-        } else {
-            console.log("INFO IN UPROFILE IS NOT NULL: ", info);
 
+        } else {
             setNewUserInfo({ ...info });
             // had to stringiny then parse so the two states will not have the same reference.
             setDbUserInfo(JSON.parse(JSON.stringify(info)));
@@ -99,23 +76,18 @@ function Uprofile({ user, info }) {
         if (Object.keys(lfSparArts).includes(mart)) {
             // remove
             setNewUserInfo((prevState) => {
-                console.log("UPDATING LF DATA");
                 setLfDataChanged(prevState => prevState + 1);
                 console.log(lfDataChanged);
                 const newState = { ...prevState }
                 delete newState.lfSparArts[mart]
-                console.log("NEWSTATE: ", newState);
                 return newState
             })
             changeUserData()
         } else {
-            console.log("MART NOT IN ARTS");
             setNewUserInfo((prevState) => {
                 const newState = { ...prevState };
-                console.log("SETTING LF DATA");
                 setLfDataChanged(prevState => prevState + 1);
                 newState.lfSparArts[mart] = true
-                console.log("NEWSTATE: ", newState);
                 return newState
             })
             changeUserData()
@@ -126,22 +98,18 @@ function Uprofile({ user, info }) {
     const updateLfCoach = (mart) => {
         if (Object.keys(lfCoachArts).includes(mart)) {
             // remove
-            console.log("MART ALREADY IN ARTS");
             setNewUserInfo((prevState) => {
                 const newState = { ...prevState }
                 setLfDataChanged(prevState => prevState + 1);
                 delete newState.lfCoachArts[mart]
-                console.log("NEWSTATE: ", newState);
                 return newState
             })
             changeUserData()
         } else {
-            console.log("MART NOT IN ARTS");
             setNewUserInfo((prevState) => {
                 const newState = { ...prevState };
                 setLfDataChanged(prevState => prevState + 1);
                 newState.lfCoachArts[mart] = true
-                console.log("NEWSTATE: ", newState);
                 return newState
             })
             changeUserData()
@@ -159,17 +127,14 @@ function Uprofile({ user, info }) {
     })
 
     const changeUserData = async () => {
-        console.log("SENDING: ", newUserInfo);
         const response = await apiService.updateUserInfo(user, newUserInfo);
         if (isEditingInfo) { setIsEditingInfo(false) }
         { showSave && setShowSave(false) }
-        console.log("RESPONSE IN UPROFILE: ", response);
     }
 
 
     // Deletes a martial art in newUserInfo
     const delMart = (mart) => {
-        console.log(`del ${mart} called`);
         setNewUserInfo((prevState) => {
             const newState = { ...prevState };
             delete newState.marts[`${mart}`]
@@ -179,7 +144,6 @@ function Uprofile({ user, info }) {
     }
     // Deletes an award in newUserInfo
     const delAward = (award) => {
-        console.log(`del ${award} called`);
         setNewUserInfo((prevState) => {
             const newState = { ...prevState };
             newState.awards = newState.awards.filter((item) => item !== award);
@@ -188,9 +152,7 @@ function Uprofile({ user, info }) {
     }
 
     const addNewInfo = (info, type = null) => {
-        console.log("INFO: ", info);
         if (type) {
-            console.log("IN FIRST");
             setNewUserInfo((prevState) => {
                 const newState = { ...prevState };
                 newState.marts = {
@@ -200,7 +162,6 @@ function Uprofile({ user, info }) {
                 return newState
             })
         } else {
-            console.log("IN SCND");
             const newAwards = { ...newUserInfo }
             newAwards.awards.push(info)
             setNewUserInfo(newAwards)
@@ -208,19 +169,14 @@ function Uprofile({ user, info }) {
     }
 
     const changeUserStatus = (makeCoach) => {
-        console.log("change user clicked");
         if (isEditingInfo) {
-            console.log("user status will be editted");
             if (makeCoach === 1 && coach === false) {
-                console.log("user status changing to coach");
                 setNewUserInfo((prevState) => {
                     const newState = { ...prevState, coach: true };
                     return newState
                 })
             }
             else if (makeCoach !== 1 && coach === true) {
-                console.log("user status changing to student");
-
                 setNewUserInfo((prevState) => {
                     const newState = { ...prevState, coach: false }
                     return newState
@@ -231,19 +187,16 @@ function Uprofile({ user, info }) {
 
     const handleEditProfile = () => {
         if (!isEditingInfo) {
-            console.log(" IN IF");
             setIsEditingInfo(!isEditingInfo)
             setShowSave(!showSave)
         } else {
             setIsEditingInfo(!isEditingInfo)
             setShowSave(!showSave)
-            console.log("SETTING NEW USER INFO TO DBUSERINFO");
             setNewUserInfo(JSON.parse(JSON.stringify({ ...dbUserInfo })))
         }
     }
 
     useEffect(() => {
-        console.log("showSave CHANGED IN UPROFILE: ", showSave);
     }, [showSave])
 
     return (
