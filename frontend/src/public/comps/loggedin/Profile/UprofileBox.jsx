f70b951e0f819e325eb83c8a915f92b00d56eeb4
Nathan <nathanflores887@gmail.com>
2023-01-29 11:46:47 +0800

    Changed width of profile contents of user profile.

--- frontend/src/public/comps/loggedin/Profile/UprofileBox.jsx
+++ frontend/src/public/comps/loggedin/Profile/UprofileBox.jsx
@@ -1,30 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faGear } from '@fortawesome/free-solid-svg-icons'
 import { useState, useMemo } from 'react'
-const showNameBio = (name, bio) => {
-    return (
-        <div className='profile'>
-            <FontAwesomeIcon icon={faUser} className="profile-avatar" />
-            <div className='profile-info'>
-                <h2>{name}</h2>
-                <p>{bio === "" || bio === undefined ? "Edit your bio" : bio}</p>
-            </div>
-        </div>
-    )
-}
 
-const showEditableNameBio = (name, bio) => {
-    return (
-        <div className='profile'>
-            <FontAwesomeIcon icon={faUser} className="profile-avatar" />
-            <div className='profile-info'>
-                <input type="text" className='i-name' name="i-name" id="" placeholder={name} />
-                <input type="text" className='i-bio' name="i-bio" id="" placeholder={bio !== "" ? bio : "Edit your bio"} />
-
-            </div>
-        </div>
-    )
-}
 
 function UprofileBox({ name, bio }) {
 
