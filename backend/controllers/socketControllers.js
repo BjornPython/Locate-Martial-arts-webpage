114e46ba12faf85a443e9a55a1f1a96aa29c2ff6
Nathan <nathanflores887@gmail.com>
2023-01-30 14:24:04 +0800

    Moved socketio to another seperate file.

---
+++ backend/controllers/socketControllers.js
@@ -0,0 +1,20 @@
+
+const makeSocket = (server) => {
+    const io = require('socket.io')(server, {
+        cors: {
+            origin: "http://localhost:5000"
+        }
+    });
+
+
+    io.on("connection", (socket) => {
+        console.log("NEW CLIENT CONNECTED IN BACKEND");
+        socket.emit("message", "MESSAGE RECEIVED?")
+    })
+
+
+}
+
+module.exports = {makeSocket}
+
+
--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,12 @@ const connectDB = require("./config/database")
 const PORT = process.env.PORT || 8000
 
 const cors = require("cors")
-
+const {makeSocket} = require("./controllers/socketControllers")
 
 connectDB()
 const app = express()
 app.use(cors())
-// app.use(cors({
-//     origin: "http://localhost:3000",
-//     "Access-Control-Allow-Origin": "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//     allowedHeaders: ["Content-Type", "Authorization"]
-// }));
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
@@ -28,14 +23,5 @@ app.use(errorHandler)
 const server = app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 });
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "http://localhost:5000"
-    }
-});
 
-io.on("connection", (socket) => {
-    console.log("NEW CLIENT CONNECTED IN BACKEND");
-    socket.emit("message", "MESSAGE RECEIVED?")
-
-})
+makeSocket(server)
