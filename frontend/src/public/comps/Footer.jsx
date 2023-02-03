9db749d2e3b3a2a8c01650a8836b99d2ab5bd389
Nathan <nathanflores887@gmail.com>
2023-02-03 14:28:32 +0800

    Finished new footer design. need to design when screen is smaller.

--- frontend/src/public/comps/Footer.jsx
+++ frontend/src/public/comps/Footer.jsx
@@ -1,5 +1,5 @@
 import "../css/footer.css"
-
+import img from "../images/locate-icon.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSquareFacebook, faSquareGithub, faSquareInstagram } from "@fortawesome/free-brands-svg-icons"
 import { faMapMarkerAlt, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons"
@@ -10,24 +10,31 @@ function Footer() {
 
             <div className="footer-main-info">
                 <div className="footer-logo">
-                    <h1>LOCATE MARTIAL ARTS</h1>
+                    <img src={img} className="brand-icn" alt="brand icon" />
+                    <div className="footer-brand ">
+                        <h3 className="mainfont">LOCATE MARTIAL ARTS</h3>
+                        <p className="font">HONE YOUR SKILLS</p>
+                    </div>
                 </div>
                 <div className="footer-info">
                     <div className="info-grp"><FontAwesomeIcon className="info-grp-icn" icon={faMapMarkerAlt} />
                         <p>#20, Sample St., Address Village, Sample City.</p>
                     </div>
                     <div className="info-grp"><FontAwesomeIcon className="info-grp-icn" icon={faPhone} />
-                        <p>+123 - 4567 - 8910</p>
+                        <p>+63 949-340-5687</p>
                     </div>
                     <div className="info-grp"><FontAwesomeIcon className="info-grp-icn" icon={faEnvelope} />
                         <p style={{ color: "yellow" }}>locatemartialarts@gmail.com</p>
                     </div>
                 </div>
-                <div className="footer-info footer-about-info">
+                <div className="footer-about-info">
                     <h4>ABOUT THE COMPANY</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Etiam odio justo, pellentesque sit amet egestas sed.</p>
                 </div>
             </div>
-
+            <p className="copyright">@Copyright. All Rights Reserved.</p>
+            <div className="footer-background"></div>
         </div >
     )
 }
